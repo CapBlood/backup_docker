5126756d1a2659ffad109b9af9901ad0ce47413e
CapBlood <stalker.anonim@mail.ru>
2020-09-01 22:58:26 +0300

    fix(setup): fixed dependencies

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='Docker Backup Utility',
       version='1.0',
       packages=['backup_utility'],
-      setup_requires=['dropbox', 'python-crontab'],
+      install_requires=['dropbox', 'python-crontab'],
       entry_points={
             'console_scripts': ['backup_docker=backup_utility.__main__:main'],
       })
