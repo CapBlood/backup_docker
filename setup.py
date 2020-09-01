471c9d8aa5cd6f69c9c60fab5336be09c896fd32
CapBlood <stalker.anonim@mail.ru>
2020-09-01 23:12:06 +0300

    feature(setup): additional metadata

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import setup
 
 setup(name='Docker Backup Utility',
+      author='CapBlood',
+      description=('This utility will allow you to make backups'
+                   'of volumes in your docker (backups are saved to your dropbox).'),
       version='1.0',
+      url='https://github.com/CapBlood/backup_docker',
+      license='GPL-3.0',
       packages=['backup_utility'],
-      install_requires=['dropbox', 'python-crontab'],
+      install_requires=['dropbox'],
+      classifiers=[
+            'Environment :: Console',
+            'Programming Language :: Python',
+            'Operating System :: Linux'
+      ],
       entry_points={
             'console_scripts': ['backup_docker=backup_utility.__main__:main'],
       })
